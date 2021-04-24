fb83a5e7fdd7e395e7d91bf31c8f6628c7884074
tamanjic <tamanji.che@metropolia.fi>
2021-04-24 17:18:25 +0300

    deleteProduct mutation resolved

--- resolvers/productResolver.js
+++ resolvers/productResolver.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import Product from '../models/Product.js';
+import Comment from '../models/Comment.js';
 import { uploadToCloudinaryAndMongoDB } from '../utils/cloudinary.js';
+import User from '../models/User.js';
 
 export default {
   Mutation: {
@@ -36,7 +38,7 @@ export default {
       try {
         const pdt = await Product.findById(args.productId);
         // verify if product exist in db for the modification
-        if (pdt) {
+        if (pdt._id) {
           return await Product.findOneAndUpdate(
             args.productId,
             { ...args },
@@ -52,6 +54,34 @@ export default {
             }
           );
         }
+      } catch (e) {
+        console.log(`modify pdt error: ${e.message}`);
+      }
+    },
+
+    deleteProduct: async (root, args) => {
+      try {
+        const pdtToDelete = await Product.findById(args.productId);
+        //  when a product is deleted, all the Comment and likes associated
+        // with the product are deleted as well
+        // and the product id is removed from the asociated author of the product.
+        if (pdtToDelete._id) {
+          // delete accociated comments
+          await Promise.all(pdtToDelete.comments.map( id => {
+            Comment.findByIdAndDelete(id);
+          }))
+
+          // dissociate author from deleted product and update User
+          const author = await User.findById(pdtToDelete.owner);
+          author.likedProducts = author.likedProducts.filter(pdt => String( pdt) !== String(pdtToDelete._id));
+          await author.save();
+
+
+          await Product.findByIdAndDelete( args.productId );
+
+          return `Successfully deleted ${pdtToDelete.title} with all its accociated likes and comments`
+        }
+        
       } catch (e) {
         console.log(`modify pdt error: ${e.message}`);
       }
