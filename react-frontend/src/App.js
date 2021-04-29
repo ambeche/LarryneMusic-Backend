c5af6dff7abf80352098de901be9b09fabfc11ef
tamanjic <tamanji.che@metropolia.fi>
2021-04-29 08:24:12 +0300

    react routing implementations

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {
+  Switch, Route, Link
+} from "react-router-dom"
+
 import {  useQuery } from '@apollo/client';
-import {PRODUCTS} from './queries';
+import {PRODUCTS} from './requests/queries';
+import Photos from './components/photos/Photos';
+import Store from './components/store/Store';
+import Login from './components/auth/Login';
+import Home from './components/home/Home';
+
 
 const App = () => {
   const result = useQuery(PRODUCTS);
@@ -9,11 +18,39 @@ const App = () => {
     return <div>loading...</div>
   }
   console.log(result.data.products);
+  const padding = {padding: 10}
   return (
     <div>
-      {result.data.products.map(p => <img alt='' src={p.image.url} width='400'/>)}
+
+        <div>
+          <Link style={padding} to="/">LarryneMusic</Link>
+          <Link style={padding} to="/photos">Photos</Link>
+          <Link style={padding} to="/store">Store</Link>
+          <Link style={padding} to="/login">Login</Link>
+        </div>
+
+        <Switch>
+          <Route path="/photos">
+            <Photos />
+          </Route>
+          <Route path="/store">
+            <Store/>
+          </Route>
+          <Route path="/login">
+            <Login  />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      
+      <div>
+        <br />
+        <em>LarryneMusic</em>
+      </div>
     </div>
   )
 }
 
 export default App
+//{result.data.products.map(p => <img alt='' src={p.image.url} width='400'/>)}
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import { 
-  ApolloClient, ApolloProvider, HttpLink, InMemoryCache
-} from '@apollo/client' 
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache
+} from '@apollo/client';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:3001/graphql',
+    uri: 'http://localhost:3001/graphql'
   })
-})
+});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
   document.getElementById('root')
-)
-
-
-
-
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
